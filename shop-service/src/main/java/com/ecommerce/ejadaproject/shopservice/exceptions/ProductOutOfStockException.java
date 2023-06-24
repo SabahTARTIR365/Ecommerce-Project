1352a848cbb58eefdc86b6e3c69cee5ee84d9066
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-06-24 22:49:18 +0300

    update the EXCEPTION structure part in the shop-service

--- shop-service/src/main/java/com/ecommerce/ejadaproject/shopservice/exceptions/ProductOutOfStockException.java
+++ shop-service/src/main/java/com/ecommerce/ejadaproject/shopservice/exceptions/ProductOutOfStockException.java
@@ -1,5 +1,9 @@
 package com.ecommerce.ejadaproject.shopservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code=HttpStatus.NOT_FOUND)
 public class ProductOutOfStockException extends Exception {
     public ProductOutOfStockException(String message) {
         super(message);
