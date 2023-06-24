5572bbbc66300ef09600b4156c6bb2dd93d9cc7e
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-06-24 22:48:54 +0300

    update the EXCEPTION structure part in the wallet-service

--- wallet-service/src/main/java/com/ecommerce/ejadaproject/walletservice/exceptions/InsufficientAmountException.java
+++ wallet-service/src/main/java/com/ecommerce/ejadaproject/walletservice/exceptions/InsufficientAmountException.java
@@ -1,5 +1,9 @@
 package com.ecommerce.ejadaproject.walletservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code=HttpStatus.PAYMENT_REQUIRED)
 public class InsufficientAmountException extends RuntimeException {
     public InsufficientAmountException(String message) {
         super(message);
