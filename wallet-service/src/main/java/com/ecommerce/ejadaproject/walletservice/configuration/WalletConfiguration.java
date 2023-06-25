e3d5bb331b998dc025e0a4f3944fc45a5e09debb
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-06-25 09:49:07 +0300

    add configration files

--- wallet-service/src/main/java/com/ecommerce/ejadaproject/walletservice/configuration/WalletConfiguration.java
+++ wallet-service/src/main/java/com/ecommerce/ejadaproject/walletservice/configuration/WalletConfiguration.java
@@ -7,4 +7,88 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("wallet-service")
 public class WalletConfiguration {
 
+	private String applicationName;
+
+	private int serverPort;
+
+	private String datasourceUrl;
+
+	private String datasourceUsername;
+
+	private String datasourcePassword;
+
+	private String jpaDdlAuto;
+
+	public String getApplicationName() {
+
+		return applicationName;
+
+	}
+
+	public void setApplicationName(String applicationName) {
+
+		this.applicationName = applicationName;
+
+	}
+
+	public int getServerPort() {
+
+		return serverPort;
+
+	}
+
+	public void setServerPort(int serverPort) {
+
+		this.serverPort = serverPort;
+
+	}
+
+	public String getDatasourceUrl() {
+
+		return datasourceUrl;
+
+	}
+
+	public void setDatasourceUrl(String datasourceUrl) {
+
+		this.datasourceUrl = datasourceUrl;
+
+	}
+
+	public String getDatasourceUsername() {
+
+		return datasourceUsername;
+
+	}
+
+	public void setDatasourceUsername(String datasourceUsername) {
+
+		this.datasourceUsername = datasourceUsername;
+
+	}
+
+	public String getDatasourcePassword() {
+
+		return datasourcePassword;
+
+	}
+
+	public void setDatasourcePassword(String datasourcePassword) {
+
+		this.datasourcePassword = datasourcePassword;
+
+	}
+
+	public String getJpaDdlAuto() {
+
+		return jpaDdlAuto;
+
+	}
+
+	public void setJpaDdlAuto(String jpaDdlAuto) {
+
+		this.jpaDdlAuto = jpaDdlAuto;
+
+	}
+
 }
