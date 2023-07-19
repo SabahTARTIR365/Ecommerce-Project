d9ebdc6f8fba72b8da703b9c9dbfde312b565e18
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-07-19 12:50:16 +0300

    test pagable thid for retriving data

--- inventory-service/src/main/java/com/ecommerce/ejadaproject/inventoryservice/services/ProductService.java
+++ inventory-service/src/main/java/com/ecommerce/ejadaproject/inventoryservice/services/ProductService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.ejadaproject.inventoryservice.beans.Product;
@@ -20,9 +22,12 @@ public class ProductService {
     }
 
     public List<Product> getAllProducts() {
-        return productRepository.findAll();//CHECK PABABLE THING
+        return productRepository.findAll();//CHECK PAGABLE THING
     }
-
+    	
+    public Page<Product> getAllProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
     public Optional<Product> getProductById(Long id) {
         return productRepository.findById(id);
     }
